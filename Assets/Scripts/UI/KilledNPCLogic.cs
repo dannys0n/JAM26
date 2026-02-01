a5d75e5c4f409e320ac8266a04c8fd187e67173b
Daihan Son <72278864+dannys0n@users.noreply.github.com>
2026-02-01 14:54:31 -0800

    feat: wip level ui logic

---
+++ Assets/Scripts/UI/KilledNPCLogic.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KilledNPCLogic : MonoBehaviour
+{
+  public Image CorrectGuessImage;
+  public Image IncorrectGuessImage;
+  public static KilledNPCLogic instance;
+
+  private void Awake()
+  {
+    instance = this;
+	}
+
+  private void Start()
+  {
+    CorrectGuessImage.enabled = false;
+    IncorrectGuessImage.enabled = false;
+  }
+
+
+	public void MadeGuess(bool correctGuess)
+  {
+    if (correctGuess)
+    {
+      CorrectGuessImage.enabled = true;
+      IncorrectGuessImage.enabled = false;
+    }
+    else
+    {
+      CorrectGuessImage.enabled = false;
+      IncorrectGuessImage.enabled = true;
+    }
+	}
+}
