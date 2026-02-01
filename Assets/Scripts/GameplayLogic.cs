0153185a077ec9d5d1a7c4a69e39325e7ff74d63
McKiwiman <kieran.mckee@digipen.edu>
2026-02-01 13:31:13 -0800

    Added more gameplay logic

--- Assets/Scripts/GameplayLogic.cs
+++ Assets/Scripts/GameplayLogic.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class GameplayLogic : MonoBehaviour
 {
+    public bool GameActive = false;
+
     [Header("Gameplay Variables")]
     public FullOutfit currentTarget;
 
@@ -26,6 +28,18 @@ public class GameplayLogic : MonoBehaviour
         PreGame();
     }
 
+    void Update()
+    {
+        if(GameActive)
+        {
+            //check for kill submit
+            if (Input.GetMouseButtonDown(2))
+            {
+                KillSubmitted();
+            }
+        }
+    }
+
     //method for setting up game variables
     public void PreGame()
     {
@@ -43,9 +57,60 @@ public class GameplayLogic : MonoBehaviour
     //method for when the start button is pressed
     public void GameStart()
     {
+        GameActive = true;
+
         //start the timer
 
         
+    }
+
+    public bool CompareOutfits(FullOutfit first, FullOutfit second)
+    {
+        if (first.Body != second.Body)
+            return false;
+        if (first.Hat != second.Hat)
+            return false;
+        if (first.Mask != second.Mask)
+            return false;
+        if (first.Full != second.Full)
+            return false;
+        if (first.Hair != second.Hair)
+            return false;
+
+        return true;
+    }
+
+    //called when the kill button is pressed or if player right clicks
+    public void KillSubmitted()
+    {
+        //check if there is an NPC being controlled
+        if(PlayerController.GetControlledPlayer() == null)
+        {
+            return;
+        }
+
+        FullOutfit killedNPC = PlayerController.GetControlledPlayer().transform.GetChild(0).GetComponent<NPCOutfit>().outfitData; 
+
+        //check to see if the data matches
+        if(CompareOutfits(killedNPC, currentTarget))
+        {
+            Debug.Log("Correct target killed");
+        }
+        else
+        {
+            Debug.Log("YOU FAILED");
+        }
+
+        //stop the timer
+
+        //freeze the scene
+
+        //zoom in
+
+        //kill the npc
+
+        //bring up results screen
+
     }
 
 
