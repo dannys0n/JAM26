33fb980e1525360badfd05449647b4cff15d7361
Daihan Son <72278864+dannys0n@users.noreply.github.com>
2026-02-01 16:02:29 -0800

    game complete scene has return btn

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -44,4 +44,9 @@ public class MainMenu : MonoBehaviour
     {
         CreditsScreen.SetActive(!CreditsScreen.activeSelf);
     }
+
+    public void BackToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+	}
 }
