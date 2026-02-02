6c6c53566ac4dc87dace2ef6ba03f22fca624345
McKiwiman <kieran.mckee@digipen.edu>
2026-02-01 16:20:54 -0800

    Update OverlayCanvasPrefab

--- Assets/Scripts/UI/FailScreen.cs
+++ Assets/Scripts/UI/FailScreen.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class FailScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("UI References")]
+    public GameObject FailPanel;
+    public TargetOutfitUI targetDisplay;
+    public TargetOutfitUI killedDisplay;
+
+    public void ShowFailScreen(FullOutfit target, FullOutfit killed)
     {
-        
+        FailPanel.SetActive(true);
+
+        //set the UI of the target outfits
+        targetDisplay.UpdateDisplay(target);
+        killedDisplay.UpdateDisplay(killed);
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitToMenu()
     {
-        
+        SceneManager.LoadScene(0);
     }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
 }
